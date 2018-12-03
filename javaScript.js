235ad893a3ace0a1cdbae6ce1b7af9dbc6060f96
Denzel97 <denzelwash97@gmail.com>
2018-12-03 10:45:40 +0300

    modified: javascript.js

--- javaScript.js
+++ javaScript.js
@@ -1,23 +1,22 @@
-function triangle(){
+var triangle = function(){
   var sideA = document.getElementById('a').value;
   var sideB = document.getElementById('b').value;
   var sideC = document.getElementById('c').value;
   var response = document.getElementById('response');
-}
 
-  if(sideA>sideB + sideC || sideB>sideA + sideC || sideC>sideA +sideB){
-    response.textContent = 'Not a triangle';
+
+  if(sideA < sideB + sideC || sideB < sideA + sideC || sideC < sideA +sideB){
+
+  if (sideA === sideB && sideB === sideC) {
+    response.textContent = 'Equilateral Triangle'
+  }
+  else if(sideA === sideB || sideB === sideC || sideA === sideC) {
+    response.textContent = 'Isosceles'
   }
   else{
-    if(sideA==sideB && sideB==sideC){
-      response.textContent = 'Equilateral triangle';
-    }
+    response.textContent = 'Scalene'
   }
-
-    else if(sideA==sideB || sideA==sideC || sideB==sideC){
-      response.textContent = 'Isosceles';
-    }
-
-    else {
-      response.textContent = 'Scalene';
-    }
+}else{
+  response.textContent = 'Not a triangle'
+}
+}
