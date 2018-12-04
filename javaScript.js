e588697641c9585aea2426a17d39a707d7bea0dc
Denzel97 <denzelwash97@gmail.com>
2018-12-04 11:07:12 +0300

    added images in javaScript.js

--- javaScript.js
+++ javaScript.js
@@ -1,23 +1,27 @@
 function triangle(){
-  var sideA = document.getElementById('a').value;
-  var sideB = document.getElementById('b').value;
-  var sideC = document.getElementById('c').value;
-  var return = document.getElementById('return')
+  var sideA =parseInt(document.getElementById('a').value);
+  var sideB =parseInt(document.getElementById('b').value);
+  var sideC =parseInt(document.getElementById('c').value);
+  var response = document.getElementById('response')
 
 
-
-   if (sideA === sideB && sideB === sideC) {
-    return.innerHTML = 'Equilateral Triangle'
+if(sideA + sideB > sideC && sideB + sideC > sideA && sideA + sideC >sideB){
+  if (sideA==sideB && sideB==sideC) {
+    response.innerHTML = 'Equilateral Triangle <img src="images/ok.png">'
   }
-  else if(sideA === sideB || sideB === sideC || sideA === sideC) {
-    return.innerHTML = 'Isosceles'
+  else if(sideA!=sideB && sideB!=sideC && sideA!=sideC) {
+    response.innerHTML = 'Scalene <img src="images/wink.png">'
   }
-  else if((sideA + sideB) > sideC || (sideB + sideC) > sideA || (sideB + sideC) > sideA && sideA !== sideB && sideA !== sideC && sideB !== sideC){
-    return.innerHTML = 'Scalene'
-  }else if ( (sideA => sideB+sideC) || (sideB => sideA+sideC) || (sideC =>sideA+sideB )){
-    return.innerHTML = 'Not a triangle'
-    // result.innerHTML = "Not a triangle <img src='images/see-no-evil.png'>"
-
+  else {
+    response.innerHTML = 'Isosceles <img src="images/thumbs-up.png">'
+  }
 }
-
-};
+else if (isNaN(sideA) || isNaN(sideB) || isNaN(sideC)){
+    response.innerHTML = 'Not a number <img src="images/hear-no-evil.png">'
+  }else{
+    response.innerHTML = 'Not a triangle <img src="images/see-no-evil.png">'
+}
+function reload(){
+  location.reload();
+}
+}
